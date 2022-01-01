21a773f996ccaabecae75ba9b5150a3e224a293b
predawn-love <1057479525@qq.com>
2022-01-01 15:31:26 +0800

    更新一下

---
+++ src/main/java/Q206ReverseLinkedList.java
@@ -0,0 +1,34 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+public class Q206ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
