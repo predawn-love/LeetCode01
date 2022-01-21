de3698e3a7817618e33cbd606f74e5ee861f6c0a
predawn-love <1057479525@qq.com>
2022-01-21 23:01:17 +0800

    2022.01.21

---
+++ src/main/java/jian_zhi_offer_special_assaults/day02/Q005.java
@@ -0,0 +1,45 @@
+package jian_zhi_offer_special_assaults.day02;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q005 {
+//    public int maxProduct(String[] words) {
+//        int res = 0;
+//        for (int i = 0; i < words.length; i++) {
+//            Set<Character> characterSet = new HashSet<>();
+//            for (char c : words[i].toCharArray()) {
+//                characterSet.add(c);
+//            }
+//loop1:      for (int j = i + 1; j < words.length; j++) {
+//                for (char c : words[j].toCharArray()) {
+//                    if (characterSet.contains(c)) {
+//                        continue loop1;
+//                    }
+//                }
+//                res = Math.max(words[i].length() * words[j].length(), res);
+//            }
+//        }
+//        return res;
+//    }
+
+
+    public int maxProduct(String[] words) {
+        int res = 0;
+        int[] wordNums = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            for (char c : words[i].toCharArray()) {
+                wordNums[i] |= (1 << (c - 'a')) ;
+            }
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if ((wordNums[i] & wordNums[j]) == 0)
+                res = Math.max(words[i].length() * words[j].length(), res);
+            }
+        }
+        return res;
+    }
+}
