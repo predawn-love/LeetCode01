1fe04b2342202d4d18d12dac23ce525e1a90b930
predawn-love <32991531+predawn-love@users.noreply.github.com>
2022-08-05 16:09:46 +0800

    Create LastRemaining.java

---
+++ src/main/java/myleetcode/jian_zhi_offer/day_24_math/LastRemaining.java
@@ -0,0 +1,11 @@
+package myleetcode.jian_zhi_offer.day_24_math;
+
+public class LastRemaining {
+    public int lastRemaining(int n, int m) {
+        int x = 0;
+        for (int i = 2; i <= n; i++) {
+            x = (x + m) % i;
+        }
+        return x;
+    }
+}
