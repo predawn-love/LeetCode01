ac41576ac8a03c8de39f13d15dcd7fb58c64bc31
predawn-love <32991531+predawn-love@users.noreply.github.com>
2022-08-05 16:47:14 +0800

    Create Q5LongestPalindrome.java

---
+++ src/main/java/myleetcode/array/Q5LongestPalindrome.java
@@ -0,0 +1,59 @@
+package myleetcode.array;
+
+public class Q5LongestPalindrome {
+    /**
+     * 三叶姐的中心扩展
+     */
+    public String longestPalindrome2(String s) {
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            // 回文串为奇数
+            int l = i - 1, r = i + 1;
+            String sub = getString(s, l, r);
+            if (sub.length() > ans.length()) ans = sub;
+
+            // 回文串为偶数
+            l = i - 1;
+            r = i + 1 - 1;
+            sub = getString(s, l, r);
+            if (sub.length() > ans.length()) ans = sub;
+        }
+        return ans;
+    }
+    String getString(String s, int l, int r) {
+        while (l >= 0 && r < s.length()
+                && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+
+
+    public String longestPalindrome(String s) {
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            // 回文串为奇数,该点为中心点
+            int l = i - 1, r = i + 1;
+            String sub = getString2(s, l, r);
+            if (sub.length() > ans.length()) ans = sub;
+
+            // 回文串为偶数，该点为左或右都可以
+            l = i - 1;
+            r = i;
+            sub = getString2(s, l, r);
+            if (sub.length() > ans.length()) ans = sub;
+        }
+        return ans;
+    }
+
+    private String getString2(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+
+
+}
