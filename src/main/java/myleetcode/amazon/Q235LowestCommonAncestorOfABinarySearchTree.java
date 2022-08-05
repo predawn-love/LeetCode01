72af68a3b8bbe77f3b9fa3360a18b5b106c18dd5
predawn-love <32991531+predawn-love@users.noreply.github.com>
2022-08-05 15:02:51 +0800

    Create Q235LowestCommonAncestorOfABinarySearchTree.java

---
+++ src/main/java/myleetcode/amazon/Q235LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,44 @@
+package myleetcode.amazon;
+
+import java.util.*;
+
+class Q235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path_p = getPath(root, p);
+        List<TreeNode> path_q = getPath(root, q);
+        TreeNode ancestor = null;
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            if (path_p.get(i) == path_q.get(i)) {
+                ancestor = path_p.get(i);
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+    public List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new ArrayList<TreeNode>();
+        TreeNode node = root;
+        while (node != target) {
+            path.add(node);
+            if (target.val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        path.add(node);
+        return path;
+    }
+
+}
+public class Q235LowestCommonAncestorOfABinarySearchTree {
+    /**
+     * 两次遍历，获取遍历的 List&lt;Node&gt;,然后比较至最后一位相同的
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+}
