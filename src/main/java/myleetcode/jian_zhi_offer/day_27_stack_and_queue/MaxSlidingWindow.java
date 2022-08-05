30f0e4609519baf35150b07c5cea0751685e2fbd
predawn-love <32991531+predawn-love@users.noreply.github.com>
2022-08-05 16:07:12 +0800

    Create MaxSlidingWindow.java

---
+++ src/main/java/myleetcode/jian_zhi_offer/day_27_stack_and_queue/MaxSlidingWindow.java
@@ -0,0 +1,34 @@
+package myleetcode.jian_zhi_offer.day_27_stack_and_queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MaxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        // 未形成窗口
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+        }
+        res[0] = deque.peekFirst();
+        // 形成窗口后
+        for (int i = k; i < nums.length; i++) {
+            if (deque.peekFirst() == nums[i - k]) {
+                deque.removeFirst();
+            }
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+    }
+}
