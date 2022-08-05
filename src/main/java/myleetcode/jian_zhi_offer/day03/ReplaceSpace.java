9f7f5a68315b0053299079a1d7f6f536a7247b05
predawn-love <32991531+predawn-love@users.noreply.github.com>
2022-08-05 16:42:41 +0800

    Create ReplaceSpace.java

---
+++ src/main/java/myleetcode/jian_zhi_offer/day03/ReplaceSpace.java
@@ -0,0 +1,8 @@
+package myleetcode.jian_zhi_offer.day03;
+
+public class ReplaceSpace {
+    public String replaceSpace(String s) {
+        String s1 = s.replaceAll(" ", "%20");
+        return s1;
+    }
+}
