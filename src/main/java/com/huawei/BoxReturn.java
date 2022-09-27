acbf7ffaaf739c5d5ac92ecb410765e7eb466579
predawn-love <32991531+predawn-love@users.noreply.github.com>
2022-09-27 14:53:01 +0800

    Create BoxReturn.java

---
+++ src/main/java/com/huawei/BoxReturn.java
@@ -0,0 +1,31 @@
+package com.huawei;
+
+import java.util.ArrayList;
+
+/**
+ * 单纯用来返回结果的实体属性类
+ */
+public class BoxReturn {
+    public int step;
+    public ArrayList<Integer> list;
+    public ArrayList<Integer> personFromList;
+
+    public BoxReturn() {
+        step = -1;
+    }
+
+    public BoxReturn(int step, ArrayList<Integer> list, ArrayList<Integer> personFromList) {
+        this.step = step;
+        this.list = list;
+        this.personFromList = personFromList;
+    }
+
+    @Override
+    public String toString() {
+        return "BoxReturn{" +
+                "step=" + step +
+                ", list=" + list +
+                ", personFromList=" + personFromList +
+                '}';
+    }
+}
